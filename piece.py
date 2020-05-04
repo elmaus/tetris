354fd32f3ea86693ab697a784cb77529966368b3
elmaus <42428075+elmaus@users.noreply.github.com>
2020-05-04 20:13:08 +0300

    Create piece.py

---
+++ piece.py
@@ -0,0 +1,90 @@
+
+pieces = []
+la = []
+lb = []
+
+l2 = [['o', 'o', 'x'],
+      ['x', 'x', 'x']]
+la.append(l2)
+l3 = [['x', 'x'],
+      ['o', 'x'],
+      ['0', 'x']]
+la.append(l3)
+l4 = [['x', 'x', 'x'],
+      ['x', 'o', 'o']]
+la.append(l4)
+l1 = [['x', 'o'],
+     ['x', 'o'],
+     ['x', 'x']]
+la.append(l1)
+pieces.append(la)
+
+
+ll2 = [['x', 'x', 'x'],
+      ['o', 'o', 'x']]
+lb.append(ll2)
+ll3 = [['x', 'x'],
+      ['x', 'o'],
+      ['x', 'o']]
+lb.append(ll3)
+ll4 = [['x', 'o', 'o'],
+      ['x', 'x', 'x']]
+lb.append(ll4)
+ll1 = [['o', 'x'],
+      ['o', 'x'],
+      ['x', 'x']]
+lb.append(ll1)
+pieces.append(lb)
+
+t = []
+t1 = [['x', 'x', 'x'],
+      ['o', 'x', 'o']]
+t.append(t1)
+t2 = [['x', 'o'],
+      ['x', 'x'],
+      ['x', 'o']]
+t.append(t2)
+t3 = [['o', 'x', 'o'],
+      ['x', 'x', 'x']]
+t.append(t3)
+t4 = [['o', 'x'],
+      ['x', 'x'],
+      ['o', 'x']]
+t.append(t4)
+pieces.append(t)
+
+sl = []
+sl2 = [['x', 'x', 'x', 'x']]
+sl.append(sl2)
+sl1 = [['x'],
+       ['x'],
+       ['x'],
+       ['x']]
+sl.append(sl1)
+pieces.append(sl)
+
+sq = []
+sq1 = [['x', 'x'],
+       ['x', 'x']]
+sq.append(sq1)
+pieces.append(sq)
+
+za = []
+za1 = [['x', 'x', 'o'],
+       ['o', 'x', 'x']]
+za.append(za1)
+za2 = [['o', 'x'],
+       ['x', 'x'],
+       ['x', 'o']]
+za.append(za2)
+pieces.append(za)
+
+zb = []
+zb1 = [['o', 'x', 'x'],
+       ['x', 'x', 'o']]
+zb.append(zb1)
+zb2 = [['x', 'o'],
+       ['x', 'x'],
+       ['o', 'x']]
+zb.append(zb2)
+pieces.append(zb)
